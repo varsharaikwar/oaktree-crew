f99931967b4947a6361239b8ed36d346a0ab25f3
Varsha Raikwar <varsha.raikwar@oaktreecloud.com>
2023-02-24 11:47:09 +0530

    add new bug list changes

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -7,33 +7,19 @@ class Ability
       can :show, Candidate
       can :index, Candidate
       can :edit, Candidate
+
     elsif user.has_role? :senior_hr
       can :index, Candidate
       can :show, Candidate
       can :edit, Candidate
+
     elsif user.has_role? :bde
       can :index, Candidate
       can :show, Candidate
+
     elsif user.has_role? :admin
       can :manage, :all
     end
-    #
-    # The first argument to `can` is the action you are giving the user 
-    # permission to do.
-    # If you pass :manage it will apply to every action. Other common actions
-    # here are :read, :create, :update and :destroy.
-    #
-    # The second argument is the resource the user can perform the action on. 
-    # If you pass :all it will apply to every resource. Otherwise pass a Ruby
-    # class of the resource.
-    #
-    # The third argument is an optional hash of conditions to further filter the
-    # objects.
-    # For example, here the user can only update published articles.
-    #
-    #   can :update, Article, :published => true
-    #
-    # See the wiki for details:
-    # https://github.com/ryanb/cancan/wiki/Defining-Abilities
   end
+  
 end
