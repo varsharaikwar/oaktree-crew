68faf8023cb67fe74e83982587ddaef4a63a571c
Varsha Raikwar <varsha.raikwar@oaktreecloud.com>
2023-03-01 16:32:27 +0530

    add user functionality on admin

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,18 +1,23 @@
 ActiveAdmin.register User do
+  permit_params [:email, :password, :password_confirmation,  role_ids: []] 
 
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
-  permit_params :email, :encrypted_password
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:email, :encrypted_password, :reset_password_token, :reset_password_sent_at, :remember_created_at]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
-  
+  form do |f|
+    f.inputs "User Details" do
+      f.input :email
+      f.input :password
+      f.input :password_confirmation
+      f.input :roles, as: :check_boxes
+    end
+    f.actions
+  end
+  controller do
+    def update
+      if params[:user][:password].blank?
+        params[:user].delete "password"
+        params[:user].delete "password_confirmation"
+      end
+
+      super
+    end
+  end
 end
