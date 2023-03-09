676f63b1cc3c12ab42079f609e0cfdd93992ad58
Varsha Raikwar <varsha.raikwar@oaktreecloud.com>
2023-03-09 15:57:48 +0530

    fix the ability file

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -8,19 +8,26 @@ class Ability
       can :index, Candidate
       can :edit, Candidate
       can :show_cities, Candidate
+      can :create, Comment
+      can :show_states, Candidate
+      can :create, Comment
       can :filter, Candidate
+      
 
     elsif user.has_role? :senior_hr
       can :index, Candidate
       can :show, Candidate
       can :edit, Candidate
+      can :create, Comment
 
     elsif user.has_role? :bde
       can :index, Candidate
       can :show, Candidate
+      can :create, Comment
 
     elsif user.has_role? :admin
       can :manage, :all
     end
   end
+  
 end
