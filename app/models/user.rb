4651292b800e42a8cd80cc3664c347eafe000d70
Varsha Raikwar <varsha.raikwar@oaktreecloud.com>
2023-04-07 11:10:54 +0530

    added validation for email in users model and improved css of candidates comment button .

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,14 @@
 class User < ApplicationRecord
+  validates :email, presence: true, uniqueness: true, format: { with: Devise.email_regexp }
+  
   rolify
+
   has_many :candidates
   has_many :comments
   has_many :leads
   has_many :notifications
   has_many :lead_assignments
+  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
