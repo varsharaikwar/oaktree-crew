67ba9a8a0b69545500a8ff31e3c16d3217afd71a
Varsha Raikwar <varsha.raikwar@oaktreecloud.com>
2023-04-14 13:41:48 +0530

    changed the format of the time in show page in details section

--- config/application.rb
+++ config/application.rb
@@ -10,8 +10,7 @@ module OaktreeCrew
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
-    config.time_zone = 'Kolkata'
-
+    
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
